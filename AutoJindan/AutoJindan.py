3e1778a602b731cb2dc2d6d2e04d168956e9253c
patrick0422 <didxodnd0422@gmail.com>
2021-04-02 16:21:40 +0900

    [UPDATE] 로그인 후 자가진단 파트 구현
    자가진단 완료 후 다음 학생으로 안넘어가지는 jot버그 발생..

--- AutoJindan/AutoJindan.py
+++ AutoJindan/AutoJindan.py
@@ -14,18 +14,31 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 
 from time import sleep
+
 #endregion
 
-#region 초기화
+
+
+#region Initialize
 URL = 'https://hcs.eduro.go.kr/#/loginHome'
 
-driver = webdriver.Chrome(executable_path='D:/Coding/python/AutoJindan/chromedriver')
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+driver = webdriver.Chrome(executable_path='D:/Coding/python/AutoJindan/chromedriver', options=options)
 driver.get(url=URL)
-driver.implicitly_wait(time_to_wait=0.5)
+# 최대 대기 시간
+driver.implicitly_wait(time_to_wait=5)
+TIME = 2
+
+NAME = '양태웅'
+DAY_OF_BIRTH = '040422'
+PASSWORD = '0422'
+
 #endregion
 
 
-#region 로그인
+
+#region Login
 # 1. 초기 페이지에서 자가진단 참여하기 버튼 클릭
 driver.find_element_by_id('btnConfirm2').click()
 
@@ -64,25 +77,125 @@ driver.find_element_by_class_name('layerFullBtn').click()
 
 
 # 4. 성명, 생년월일 선택
-driver.find_element_by_id('user_name_input').send_keys('양태웅')
-driver.find_element_by_id('birthday_input').send_keys('040422')
+driver.find_element_by_id('user_name_input').send_keys(NAME)
+driver.find_element_by_id('birthday_input').send_keys(DAY_OF_BIRTH)
 # 제출
 driver.find_element_by_id('btnConfirm').click()
 #비밀번호 입력 페이지로 넘어가는 동안 잠깐 멈춰줘야 하는데, 암묵적 대기가 먹지 않으니 그냥 sleep으로 멈춤
-sleep(2)
+sleep(TIME)
 
 
 
 # 5. 비밀번호 선택
-password = driver.find_element_by_class_name('input_text_common')
-password.send_keys('0422')
-
+driver.find_element_by_class_name('input_text_common').send_keys(PASSWORD)
 # 제출
 driver.find_element_by_xpath('//*[@id="btnConfirm"]').click()
+
+sleep(TIME)
+
 #endregion
 
 
-sleep(5)
+
+#region 자가진단
+#학생 리스트 가져오기
+items = driver.find_element_by_xpath('//*[@id="container"]/div/section[2]/div[2]/ul').find_elements_by_tag_name('li')
+print(f'확인된 학생 수 : {len(items)}명')
+
+
+
+
+
+
+#region SAVED
+# 학생 수 만큼 루프
+# for item in items:
+#     sleep(3)
+#     # 자가진단 진입
+#     try:
+#         name = item.find_element_by_class_name('name').get_attribute('innerHTML')
+#         item.find_element_by_class_name('name').click()
+        
+#         # 학생이 이미 참여 했을 경우 발생하는 경고창 처리
+#         try:
+#             print(f'{name}(은)는 이미 참여하였습니다')
+#             driver.switch_to.alert().dismiss()
+#         except:
+#             pass
+
+#         sleep(3)
+
+#         # 조사 응답
+#         driver.find_element_by_xpath('//*[@id="survey_q1a1"]').click()
+#         driver.find_element_by_xpath('//*[@id="survey_q2a1"]').click()
+#         driver.find_element_by_xpath('//*[@id="survey_q3a1"]').click()
+
+#         sleep(1)
+
+#         # 응답 제출
+#         driver.find_element_by_id('btnConfirm').click()
+
+#         sleep(3)
+#         # 처음으로 버튼 클릭
+#         driver.find_element_by_xpath('/html/body/app-root/div/div[1]/div[1]/ul/li/a').click()
+#     except selenium.common.exceptions.StaleElementReferenceException as err:
+#         print(f'[StaleElementReferenceException] Passing {name}')
+#         pass
+#endregion
+
+
+
+
+
+i = 0
+
+for i in range(0, len(items)):
+    print(f'확인된 학생 수 : {len(items)}명')
+    sleep(3)
+    try:
+        i += 1
+        # 자가진단 진입
+        name = items[i].find_element_by_class_name('name').get_attribute('innerHTML') 
+        items[i].find_element_by_class_name('name').click()
+        
+        # 학생이 이미 참여 했을 경우 발생하는 경고창 처리
+        try:
+            driver.switch_to.alert.dismiss()
+            print(f'{i}.{name}(은)는 이미 자가진단이 완료되었습니다.')
+        except:
+            sleep(3)
+
+            # 조사 응답
+            driver.find_element_by_xpath('//*[@id="survey_q1a1"]').click()
+            driver.find_element_by_xpath('//*[@id="survey_q2a1"]').click()
+            driver.find_element_by_xpath('//*[@id="survey_q3a1"]').click()
+
+            sleep(1)
+
+            # 응답 제출
+            driver.find_element_by_id('btnConfirm').click()
+
+            print(f'{name} 제출 완료')
+
+            sleep(3)
+            # 처음으로 버튼 클릭
+            driver.find_element_by_xpath('/html/body/app-root/div/div[1]/div[1]/ul/li/a').click()
+
+    except selenium.common.exceptions.StaleElementReferenceException as err:
+        print(f'[StaleElementReferenceException] Passing {name}')
+        pass
+
+
+
+
+#endregion
+
+
+
+sleep(TIME)
+
+
 
 # 브라우저 닫기
-# driver.close()
+print('\nTERMINATING!!')
+driver.close()
